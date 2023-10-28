023571547f42505f590f8959ec501ff01df83598
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2023-10-28 11:22:28 -0700

    new updates

--- ip_update.py
+++ ip_update.py
@@ -8,13 +8,16 @@ from requests import get
 import smtplib, ssl
 from email.message import EmailMessage
 from datetime import datetime
+from sys import platform
+import sys
+
 
 def check_ip ():
     ip = get('https://api.ipify.org').text
     # print('My public IP address is: {}'.format(ip))
     return ip
 
-def send_email (currentIP,date):
+def send_email (currentIP,now,os):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
     sender_email = "tadeubanzato@gmail.com"  # Enter your address
@@ -22,7 +25,7 @@ def send_email (currentIP,date):
     password = "qqzugdeqjpvzvoge"
 
     msg = EmailMessage()
-    msg.set_content(f"Hyotoko's IP was updated by the ISP, make sure to use the most updated version on your VPN.\n\nLast check was: {date}\nNew IP is: {currentIP}\n\n")
+    msg.set_content(f"Hyotoko's IP was updated by the ISP, make sure to use the most updated version on your VPN.\n\nInformation checked from {os}\nLast updated check was: {now}\nNew IP is: {currentIP}\n\nUpdate checks happens every 2 hours, the IP might have updated sooner.")
     msg['Subject'] = f"📡 Hyotoko IP just got updated"
     msg['From'] = sender_email
     msg['To'] = receiver_email
@@ -42,16 +45,31 @@ def current_data ():
     f.close()
     return data
 
+def osCheck ():
+    if sys.platform.startswith('linux'):
+        os = "Linux"
+    elif sys.platform.startswith('darwin'):
+        os = "MacOS"
+    elif sys.platform.startswith('win32'):
+        os = "Windows"
+    return os
+
 
 if __name__ == '__main__':
     data = current_data()
     currentIP = check_ip ()
+    os = osCheck ()
 
-    if currentIP != data['old-ip']:
-        send_email (currentIP,str(datetime.now()))
-        data['last-check'] = str(datetime.now())
-        # data['old-ip'] = 'test'
-        data['old-ip'] = currentIP
+    now = str(datetime.now())
 
-        with open('data.json', 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
+    data['check-status']['last-check'] = now
+    data['check-status']['os'] = os
+
+    if currentIP != data['update-status']['old-ip']:
+        send_email (currentIP,now,os)
+        data['update-status']['last-update'] = now
+        data['update-status']['old-ip'] = currentIP
+
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
