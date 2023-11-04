42bc1c46953dc0d507ecc68ff191b4f6c1f08fa3
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2023-11-04 09:27:08 -0700

    update

--- ip-process.sh
+++ ip-process.sh
@@ -2,5 +2,5 @@
 ## ip-process.sh
 ## OBS: Make sure to > chmod u+x general-bash.sh
 
-cd /home/tbanzato/python/IP_update/
+cd /home/tbanzato/Python/IP_update/
 python3 ip_update.py
--- ip_update.py
+++ ip_update.py
@@ -121,3 +121,7 @@ if __name__ == '__main__':
 
     with open('data.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
+
+
+
+    from geopy.geocoders import Nominatim
