95b08d893e2cc2956082a22fd67f47f5d3924397
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2024-03-07 18:33:55 -0800

    update
    Added Push Notification functionality using Pushover

--- push_notification.py
+++ push_notification.py
@@ -5,9 +5,9 @@
 import requests
 
 def send_push(user,token,currentIP,date,os,location):
-    payload = {"message": f'New IP for Hyotoko in {location.city} - {currentIP} updated {date}', "user": user, "token": token}
+    payload = {"message": f'New IP for Hyotoko in {location.city} - {currentIP} updated {date.strftime("%D")}', "user": user, "token": token}
     requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'This is a test'})
-    print(f'New IP for Hyotoko in {location.city} - {currentIP} updated {date}')
+    print(f'New IP for Hyotoko in {location.city} - {currentIP} updated {date.strftime("%D")}')
 
 
 # ### IFTTT
@@ -18,5 +18,3 @@ def send_push(user,token,currentIP,date,os,location):
 # url = 'https://maker.ifttt.com/trigger/{e}/with/key/{k}/'.format(e=event,k=api_key)
 # payload = {'value1': 'Hyotoko Notification', 'value2': 'value 2', 'value3': 'value3'}
 # requests.post(url, data=payload)
-
-
