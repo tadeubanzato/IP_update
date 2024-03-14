99ef7cf157f2d947ca7de9c7ce0eecf27420c2cb
Tadeu Banzato <10622335+tadeubanzato@users.noreply.github.com>
2024-03-13 20:18:25 -0700

    Update push_notification.py

--- push_notification.py
+++ push_notification.py
@@ -2,7 +2,7 @@ import requests
 
 def send_push(user,token,currentIP,date,os,location):
     print(f'Sending Push Notification to Pushover')
-    payload = {"message": f'New Hyotoko IP: {currentIP}\nLocation: {location['city']} last update: {date.strftime("%D")}', "user": user, "token": token}
+    payload = {"message": f'New Hyotoko IP: {currentIP}\nLocation: {location["city"]} last update: {date.strftime("%D")}', "user": user, "token": token}
     requests.post('https://api.pushover.net/1/messages.json', data=payload, headers={'User-Agent': 'This is a test'})
 
 
